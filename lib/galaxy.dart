385aab1b5f6396380cbef896713b0b3e634b7875
devsheder <devsheder@gmail.com>
2018-10-19 14:30:58 +0200

    step 2

---
+++ lib/galaxy.dart
@@ -0,0 +1,13 @@
+class Galaxy {
+  final String id;
+  final String title;
+  final String previewHref;
+  final String description;
+
+  Galaxy({
+    this.id,
+    this.title,
+    this.previewHref,
+    this.description,
+  });
+}
